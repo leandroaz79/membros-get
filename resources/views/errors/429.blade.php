96c909125e13518bb0d58d8d917a9979cdaa081f
leandroaz79 <leandroaz@gmail.com>
2026-09-03 11:23:48 -0300

    Fix Geral

--- resources/views/errors/429.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors::minimal')
-
-@section('title', __('Too Many Requests'))
-@section('code', '429')
-@section('message', __('Muitas tentativas. Aguarde 1 minuto e tente novamente, ou use o link do e-mail de compra.'))
